f315beb5142cbdd4a71ff30fa1f0ea14e59ea39c
Michael-Seth <michaelseth78@gmail.com>
2022-06-15 03:01:58 +0100

    dark mode implementation

--- js/main.js
+++ js/main.js
@@ -146,20 +146,62 @@ for (var i = 0; i < navs.length; i++) {
   );
 }
 
-// // Dark Mode
-// const options = {
-//   bottom: "20px", // default: '32px'
-//   right: "20px", // default: '32px'
-//   left: "unset", // default: 'unset'
-//   time: "0.5s", // default: '0.3s'
-//   mixColor: "#fff", // default: '#fff'
-//   backgroundColor: "#fff", // default: '#fff'
-//   buttonColorDark: "#100f2c", // default: '#100f2c'
-//   buttonColorLight: "#fff", // default: '#fff'
-//   saveInCookies: false, // default: true,
-//   label: "🌓", // default: ''
-//   autoMatchOsTheme: true, // default: true
-// };
+const page = document.querySelector("body");
+const mode = document.querySelector("b");
+const toggle = page.querySelector(".toggle-input");
+//const toggleIcon = page.querySelector(".toggl-icon");
 
-// const darkmode = new Darkmode(options);
-// darkmode.showWidget();
+// set theme and localStorage on page load
+setCheckedState();
+
+function setCheckedState() {
+  // checks if localStorage has a "checked" value set at all
+  if (!(localStorage.checked === undefined)) {
+    // if it does, it sets the state of the toggle accordingly
+    toggle.checked = isTrue(localStorage.getItem("checked"));
+    // after setting the toggle state, the theme is adjusted according to the checked state
+    toggleTheme();
+  }
+}
+
+function toggleTheme() {
+  // Toggle theme based on state of checkbox
+  replaceClass();
+  // replace icons on page
+  // toggleIconTheme();
+  // set the value of the "checked" key in localStorage
+  updateLocalStorage();
+}
+
+function replaceClass() {
+  if (toggle.checked) {
+    page.classList.replace("light", "dark");
+    mode.textContent = "Dark-mode";
+  } else {
+    page.classList.replace("dark", "light");
+    mode.textContent = "Light-mode";
+  }
+}
+
+// function toggleIconTheme() {
+//   // Replace icons not able to be targeted by css variables
+//   if (page.classList.contains("light")) {
+//     toggleIcon.src = "./images/moon.svg";
+//     toggleIcon.alt = "Switch to Dark Mode";
+//   } else {
+//     toggleIcon.src = "./images/sun.svg";
+//     toggleIcon.alt = "Switch to Light Mode";
+//   }
+// }
+
+function updateLocalStorage() {
+  localStorage.setItem("checked", toggle.checked);
+}
+
+function isTrue(value) {
+  // convert string to boolean
+  return value === "true";
+}
+
+// Toggle theme any time the state of the checkbox changes
+toggle.addEventListener("change", toggleTheme);
